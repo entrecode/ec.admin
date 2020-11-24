c3b41f97c66f5f60f4a4eeb11a374916f661bebd
Felix Roos <roos@entrecode.de>
2020-11-24 15:35:30 +0100

    added demo + mdx-deck slides

---
+++ demo/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Admin, Resource, Loading } from 'react-admin';
+import { useSession, useDatamanager, entryCrud } from '../lib';
+
+const App = () => {
+  const dataProvider = useDatamanager('73538731-4ac3-4a1a-b3b5-e31d09e94d42');
+  const authProvider = useSession();
+  if (!dataProvider || !authProvider) {
+    return <Loading />;
+  }
+  return (
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+      <Resource name={'muffin'} {...entryCrud} />
+      <Resource name={'baker'} {...entryCrud} />
+      <Resource name={'field_test'} {...entryCrud} />
+    </Admin>
+  );
+};
+export default App;
