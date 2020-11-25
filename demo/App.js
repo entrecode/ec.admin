da7ecc74cd7c62939d04734c5aa9cbfeddd86a5b
Felix Roos <roos@entrecode.de>
2020-11-25 12:08:23 +0100

    proper theme

--- demo/App.js
+++ demo/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Admin, Resource, Loading } from 'react-admin';
-import { useSession, useDatamanager, entryCrud } from '../lib';
+import { useSession, useDatamanager, entryCrud, themes } from '../lib';
 
 const App = () => {
   const dataProvider = useDatamanager('73538731-4ac3-4a1a-b3b5-e31d09e94d42');
@@ -9,7 +9,7 @@ const App = () => {
     return <Loading />;
   }
   return (
-    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Admin dataProvider={dataProvider} authProvider={authProvider} theme={themes.light}>
       <Resource name={'muffin'} {...entryCrud} />
       <Resource name={'baker'} {...entryCrud} />
       <Resource name={'field_test'} {...entryCrud} />
